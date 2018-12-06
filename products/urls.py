9e11fc58d3e52f8854db15f85d95ad73617bc869
Vishal Tyagi <viishaltyagi@gmail.com>
2018-12-06 13:11:13 -0500

    Refining Products URLs

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (
+    productListView,
+    productDetailSlugView,
+    )
+
+urlpatterns = [
+    path('',productListView.as_view()),
+    # path('products/<pk>',productDetailSlugView.as_view()),
+    path('<slug>',productDetailSlugView.as_view()),
+]
